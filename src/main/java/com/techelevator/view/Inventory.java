b0b6b7c45c33c5b699e31b3379e817f876077796
Richard Ta <richardlamta@gmail.com>
2023-06-08 10:25:24 -0700

    Items.java

--- src/main/java/com/techelevator/view/Inventory.java
+++ src/main/java/com/techelevator/view/Inventory.java
@@ -1,13 +1,14 @@
 package com.techelevator.view;
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.io.File;
 import java.sql.Array;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class Inventory {
+    private static Map<String, Integer> inventoryMap= new HashMap<>();
+
 
     private static ArrayList<String> inventory = new ArrayList<String>();
     public static void main(String[] args) {
@@ -20,22 +21,30 @@ public class Inventory {
         } catch (Exception ex) {
             System.out.println("Something went wrong fetching inventory");
         }
-
-    }
-    public static Map<String, Integer> getInventory() {
-        Map<String, Integer> inventoryMap= new HashMap<>();
         for (String item : inventory) {
             String[] itemArray = item.split("\\|");
             inventoryMap.put(itemArray[0], 5);
         }
+
+
+        System.out.println(getItems());
+
+
+    }
+    public static Map<String, Integer> getInventory() {
+        //Map<String, Integer> inventoryMap= new HashMap<>();
         return inventoryMap;
     }
 
+    //code, quantity
     public static void setInventory(String code) {
-
+        int newValue = inventoryMap.get(code);
+        inventoryMap.put(code, newValue - 1);
+        System.out.println(inventoryMap);
     }
 
     public static ArrayList<String> getItems() {
         return inventory;
     }
+
 }
---
+++ src/main/java/com/techelevator/view/Item.java
@@ -0,0 +1,22 @@
+package com.techelevator.view;
+
+import java.util.ArrayList;
+
+public class Item extends Inventory{
+
+    private String code;
+    private String name;
+    private int price;
+    private String category;
+
+    public void Item(String code) {
+        //create for loop to iterate through the code to get each value/value type
+        ArrayList<String> inventory = (super.getItems());
+//        for (int i=0; i<inventory.size(); i++) {
+//            if (code == inventory[i]
+//            }
+        }
+
+
+    }
+}
