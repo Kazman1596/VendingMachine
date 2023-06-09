aaa3b3da098c7b145e9ad955051d62aa7b4e2397
Stephen Kaczmarowski <srkaz94@gmail.com>
2023-06-09 10:05:42 -0700

    added purchaseMenu class

--- src/main/java/com/techelevator/VendingMachineCLI.java
+++ src/main/java/com/techelevator/VendingMachineCLI.java
@@ -3,6 +3,7 @@ package com.techelevator;
 
 import com.techelevator.view.Inventory;
 import com.techelevator.view.Menu;
+import com.techelevator.view.PurchaseMenu;
 import com.techelevator.view.Transaction;
 
 import java.util.Scanner;
@@ -12,18 +13,15 @@ public class VendingMachineCLI {
 	private static final String MAIN_MENU_OPTION_PURCHASE = "Purchase";
 	private static final String MAIN_MENU_OPTION_EXIT = "Exit";
 	private static final String MAIN_MENU_SECRET_OPTION = "*Sales Report";
-	private static final String PURCHASE_MENU_OPTION_FEED_MONEY = "Feed Money";
-	private static final String PURCHASE_MENU_OPTION_SELECT_PRODUCT = "Select Product";
-	private static final String PURCHASE_MENU_OPTION_FINISH_TRANSACTION = "Finish Transaction";
 
 	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE, MAIN_MENU_OPTION_EXIT, MAIN_MENU_SECRET_OPTION };
 
-	private static final String[] PURCHASE_MENU_OPTIONS = { PURCHASE_MENU_OPTION_FEED_MONEY, PURCHASE_MENU_OPTION_SELECT_PRODUCT, PURCHASE_MENU_OPTION_FINISH_TRANSACTION};
 
 	Scanner scanner = new Scanner(System.in);
 	private Menu menu;
 	Transaction transaction = new Transaction();
 	Inventory inventory = new Inventory();
+	PurchaseMenu purchaseMenu = new PurchaseMenu();
 
 
 	public VendingMachineCLI(Menu menu) {
@@ -35,24 +33,8 @@ public class VendingMachineCLI {
 
 			if (choice.equals(MAIN_MENU_OPTION_DISPLAY_ITEMS)) {
 				inventory.displayInventory();
-
 			} else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
-				String purchaseChoice = (String) menu.getChoiceFromOptions(PURCHASE_MENU_OPTIONS);
-				if (purchaseChoice.equals(PURCHASE_MENU_OPTION_FEED_MONEY)) {
-					System.out.println("Input $(1), $(5), or $(10) bill please:");
-					String moneyInput = scanner.nextLine();
-					transaction.setBalance(moneyInput);
-					transaction.displayBalance();
-				} else if (purchaseChoice.equals(PURCHASE_MENU_OPTION_SELECT_PRODUCT)) {
-
-					inventory.displayInventory();
-					System.out.println("Please enter item code: ");
-					String userInput = scanner.nextLine();
-					transaction.dispenseItem("B4");
-
-				} else if (purchaseChoice.equals(PURCHASE_MENU_OPTION_FINISH_TRANSACTION)) {
-					break;
-				}
+				purchaseMenu.runPurchaseMenu();
 			} else if (choice.equals(MAIN_MENU_OPTION_EXIT)) {
 				break;
 			}
